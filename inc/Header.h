3f79b5546446dfc6f48950e49d8f60a52bb033ad
Jerome Babu C <72558625+jerryd76@users.noreply.github.com>
2022-04-07 10:29:33 +0530

    Update Header.h

--- inc/Header.h
+++ inc/Header.h
@@ -19,24 +19,24 @@ class Header{
 		char* GetBaseAddress();			// gets the base adddress of the boot image
 		char* GetChecksum();			// gives the check which has been genrated earlier
 		char* GetBIAddress();			// gives the boot image address
-		char* GetBISize();				// gives the size of the boot image
+		char* GetBISize();			// gives the size of the boot image
 		char* GetBIVersion();			// gets the version of the boot image
 	
 	//functions	
 	public:
 		int ParseDetails(string file_name);		// function to fetch details about the boot image file
-		void DisplayDetails();					// function to display the details of the boot image file
-		string GetFileType(string file_name);	// function to get the file type 
+		void DisplayDetails();				// function to display the details of the boot image file
+		string GetFileType(string file_name);	        // function to get the file type 
 		
 	private:
 		char device_id[DEVICE_ID_SIZE];			// char array to hold device id
 		char magic_no[MAGIC_NO_SIZE];			// char array to hold magic no
-		char device_name[MAX];					// char array to hold device name	
-		char base_address[MAX];					// char array to hold base address
-		char checksum[MAX];						// char array to hold checksum value
-		char bootImageAddress[MAX];				// char array to hold boot Image address
-		char bootImageSize[MAX];				// char array to hold boot Image size
-		char bootImageVersion[MAX];				// char array to hold boot image version
+		char device_name[MAX];				// char array to hold device name	
+		char base_address[MAX];				// char array to hold base address
+		char checksum[MAX];				// char array to hold checksum value
+		char bootImageAddress[MAX];			// char array to hold boot Image address
+		char bootImageSize[MAX];			// char array to hold boot Image size
+		char bootImageVersion[MAX];			// char array to hold boot image version
 };
 
 #endif
